20e9df99679712c4c8c714b66c0999cffcc3de6a
atttaskalyar <40478931+atttaskalyar@users.noreply.github.com>
2023-06-19 07:13:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -132,67 +132,6 @@ const generateGalaxy = () => {
    */
   // geometry = new THREE.BufferGeometry(); 
 
-  // const positions = new Float32Array(parameters.count * 3);
-  // const colors = new Float32Array(parameters.count * 3);
-  // const scales = new Float32Array(parameters.count);
-  // const randomness = new Float32Array(parameters.count * 3);
-  // const insideColor = new THREE.Color(parameters.insideColor);
-  // const outsideColor = new THREE.Color(parameters.outsideColor);
-
-  // for (let i = 0; i < parameters.count; i++) {
-  //   const i3 = i * 3;
-
-  //   // Position
-  //   const radius = Math.random() * parameters.radius;
-
-  //   const branchAngle =
-  //     ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
-
-  //   const randomX =
-  //     Math.pow(Math.random(), parameters.randomnessPower) *
-  //     (Math.random() < 0.5 ? 1 : -1) *
-  //     parameters.randomness *
-  //     radius;
-  //   const randomY =
-  //     Math.pow(Math.random(), parameters.randomnessPower) *
-  //     (Math.random() < 0.5 ? 1 : -1) *
-  //     parameters.randomness *
-  //     radius;
-  //   const randomZ =
-  //     Math.pow(Math.random(), parameters.randomnessPower) *
-  //     (Math.random() < 0.5 ? 1 : -1) *
-  //     parameters.randomness *
-  //     radius;
-
-  //   positions[i3] = Math.cos(branchAngle) * radius;
-  //   positions[i3 + 1] = 0;
-  //   positions[i3 + 2] = Math.sin(branchAngle) * radius;
-
-  //   // Randomness
-  //   randomness[i3] = randomX;
-  //   randomness[i3 + 1] = randomY;
-  //   randomness[i3 + 2] = randomZ;
-
-  //   // Color
-  //   const mixedColor = insideColor.clone();
-  //   mixedColor.lerp(outsideColor, radius / parameters.radius);
-
-  //   colors[i3] = mixedColor.r;
-  //   colors[i3 + 1] = mixedColor.g;
-  //   colors[i3 + 2] = mixedColor.b;
-
-  //   // Scales
-  //   scales[i] = Math.random();
-  // }
-
-  // geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
-  // geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
-  // geometry.setAttribute("aScale", new THREE.BufferAttribute(scales, 1));
-  // geometry.setAttribute(
-  //   "aRandomness",
-  //   new THREE.BufferAttribute(randomness, 3)
-  // );
-  // console.log(new THREE.)
   /**
    * Material
    */
